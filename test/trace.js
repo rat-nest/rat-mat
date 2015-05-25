d9eb1b8c31f1a3a4b06a7647c9fb796306706be7
mikolalysenko <mikolalysenko@gmail.com>
2015-05-25 12:42:41 -0500

    stub for trace

---
+++ test/trace.js
@@ -0,0 +1,10 @@
+'use strict'
+
+var tape = require('tape')
+var brEquals = require('big-rat/equals')
+var mat = require('../index')
+var trace = require('../trace')
+
+tape('matrix trace', function(t) {
+  t.end()
+})
